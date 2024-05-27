11ae10f1f4e7bafea7bd22473924165b21330830
Peter Walter Tafirenyika Chifamba <PeterWalter@users.noreply.github.com>
2024-05-27 11:14:02 +0200

    update files

--- obj/Debug/net8.0-windows/ReadTerritoriumFiles.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ReadTerritoriumFiles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReadTerritoriumFiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3239ceeccdf25c2886c340c209199dff61ea7ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27bb0856862af8fd88ec3ba86530c4545ff7c086")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReadTerritoriumFiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReadTerritoriumFiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
